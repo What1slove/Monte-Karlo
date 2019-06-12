66bea21fcbf69d5298e58df2788b8c4801b10013
VladislavKondyrev <vkondyrev1474@gmail.com>
2019-06-12 19:29:11 +0700

    Сделанно

--- Frontend12/wwwroot/scripts.js
+++ Frontend12/wwwroot/scripts.js
@@ -1,1 +1,11 @@
-﻿
+﻿var button = $("#result-button");
+var formulInput = $("#formul");
+var firstedgeInput = $("#edge1");
+var secondedgeInput = $("#edge2");
+var answerOutput = $("#answer");
+button.on("click", main);
+
+function main() {
+    alert(formulInput.val() + " " + firstedgeInput.val()+ " " + secondedgeInput.val());
+
+}
